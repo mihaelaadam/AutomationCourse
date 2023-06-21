660182c46ab6b6e9b0742e760ea2d9cc46c5927b
mihaelaadam <adam_mihaela@yahoo.com>
2023-06-21 20:39:58 +0300

    initial commit

--- src/main/java/course/course03/Main.java
+++ src/main/java/course/course03/Main.java
@@ -6,13 +6,13 @@ public class Main {
         // null object. Throws NullPointerException
 //        Circle circle = null;
 //        System.out.println(circle.radius);
-        //new object
-        Circle circle = new Circle();
-        circle.setRadius(2.0);
-        System.out.println(circle.radius);
+                //new object
+                Circle circle = new Circle();
+                circle.setRadius(2.0);
+                System.out.println(circle.radius);
 //        System.out.println(circle.getArea());
-        circle.setRadius(5.0);
-        System.out.println(circle.radius);
-        System.out.println(circle.getArea());
-    }
+                circle.setRadius(5.0);
+                System.out.println(circle.radius);
+                System.out.println(circle.getArea());
+        }
 }

8adbe9add91602723ba4dfd311c58b5a993709d3
mihaelaadam <adam_mihaela@yahoo.com>
2023-06-22 16:09:20 +0300

    initial commit

--- src/main/java/homework/homework03/Library.java
+++ src/main/java/homework/homework03/Library.java
@@ -2,11 +2,11 @@ package homework.homework03;
 
 public class Library {
     public static void main(String[] args) {
-        String name;
-        String email;
-        String title;
-        int year;
-        double price;
+        String name = "Bernard Werber";
+        String email = "bernard.werber@gmail.com";
+        String title = "Ziua furnicilor";
+        int year = 1996;
+        double price = 10;
         Author author = new Author(name, email);
         Book book = new Book(title, year, author, price);
         System.out.println("Book " + book.getTitle() + ", (" + book.getPrice() + " RON) , by " + author.getName() + ", published in " + book.getYear());

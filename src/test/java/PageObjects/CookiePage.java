d5d9234c3a1a96bd922006654fbd49783de32866
mihaelaadam <adam_mihaela@yahoo.com>
2023-08-09 22:36:13 +0300

    initial commit

--- src/test/java/PageObjects/CookiePage.java
+++ src/test/java/PageObjects/CookiePage.java
@@ -4,11 +4,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CookiePage {
     @FindBy(id = "set-cookie")
     private WebElement setCookieButtonElement;
+    @FindBy(id = "cookie-value")
+    private WebElement cookieValueElement;
+
+    @FindBy(id = "delete-cookie")
+    private WebElement removeCookieButtonElement;
+
+    @FindBy(tagName = "h1")
+    private WebElement pageTitleElement;
     WebDriver driver;
     WebDriverWait wait;
     public CookiePage(WebDriver driver) {    //  constructor la driver ca sa instantiem page login - recomandat
@@ -20,6 +29,26 @@ public class CookiePage {
     }
 
     public boolean setCookieButtonIsDisplayed() {
+        wait.until(ExpectedConditions.visibilityOf(setCookieButtonElement));
         return setCookieButtonElement.isDisplayed();
+    }
+    public void clickSetCookieButton() {
+        setCookieButtonElement.click();
+    }
+
+    public String getDisplayedCookie() {
+        return cookieValueElement.getText();
+    }
+
+    public void removeCookie() {
+        removeCookieButtonElement.click();
+    }
+
+    public boolean cookieValueIsDisplayed() {
+        return cookieValueElement.isDisplayed();
+    }
+
+    public String getPageTitle() {
+        return pageTitleElement.getText();
     }
 }
